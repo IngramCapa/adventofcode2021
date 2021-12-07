71f53b53e4a089eeab7d2d07623e3080f9e41d5e
Raluca Ciucu <raluca.ciucu@gmail.com>
2021-12-07 12:07:18 +0000

    part 2 day 2

--- 02/index.js
+++ 02/index.js
@@ -1,15 +1,36 @@
 const fs = require('fs')
 const instructions = fs.readFileSync('./02/data.txt').toString().split('\n')
 
-let directionValue = 0
-let depthValue = 0
+//Solution to part 2
+
+let position = 0
+let depth = 0
+let aim = 0
 
 instructions.forEach((instruction) => {
   const direction = instruction.split(' ')[0]
   const length = parseInt(instruction.split(' ')[1])
 
-  if (direction === 'forward') return (directionValue += length)
-  direction === 'down' ? (depthValue += length) : (depthValue -= length)
+  if (direction === 'forward') {
+    position += length
+    return (depth += length * aim)
+  }
+  direction === 'down' ? (aim += length) : (aim -= length)
 })
 
-return directionValue * depthValue
+return position * depth
+
+//Solution to part 1
+
+let direction = 0
+let depth = 0
+
+instructions.forEach((instruction) => {
+  const direction = instruction.split(' ')[0]
+  const length = parseInt(instruction.split(' ')[1])
+
+  if (direction === 'forward') return (direction += length)
+  direction === 'down' ? (depth += length) : (depth -= length)
+})
+
+return direction * depth
